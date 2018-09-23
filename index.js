5b14b3a76776c585aecb7df40c38b32f9c1735ce
teqodoo <ravisk1514@gmail.com>
2018-09-23 21:18:10 +0530

    testing device config with MI A1

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+// Index.android.js - place code in here for android.
 
-AppRegistry.registerComponent(appName, () => App);
+// Import a library to hrlp create a component.
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create the component
+const App = () => (
+    <Header />
+);
+
+// Render it on the device.
+AppRegistry.registerComponent('albums', () => App);
+
+
+
+
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+// import { name as appName } from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+const Header = () => {
+    return (
+        <View>
+                                            <Text>Welcome to the First React Native App</Text>
+        </View>
+    );
+};
+
+
+export default Header;
